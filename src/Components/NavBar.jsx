82c7ce31b25cf3557937db104ed824ae08144470
owaisghaira <owais.ghaira99@gmail.com>
2021-06-17 23:26:46 +0500

    shoe store done

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import Logo from '../images/logo2.jpg'
 function NavBar() {
     return (
         <div>
 
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Shoe Store</a>
+                <img width='50' src={Logo}></img>
+                <Link to='/' className="navbar-brand" href="#">Shoe Store</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
+                    <ul className="navbar-nav ml-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <Link to='/' className="nav-link" href="#">Home <span className="sr-only">(current)</span></Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
+                            <Link to='/product' className="nav-link" href="#">Products</Link>
                         </li>
                     </ul>
                 </div>
--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import NavBar from '../Components/NavBar'
+import Messi from '../images/messi-min.png'
 function Home() {
     return (
-        <div>
-            <NavBar/>
-                <h1>Home</h1>
-        </div>
+        <>
+            <NavBar />
+            <div className='container-fluid ' style={{backgroundColor:'black'}}>
+                <div className='row '>
+                    <div className='col-lg-5 ml-5 col-md-5 col-12'>
+                        <img width='350' src={Messi}></img>
+                    </div>
+                    <div className='col-lg-5 col-md-5 m-5 p-5 col-12' style={{color:'white'}} >
+                        <h1>BUILT FOR YOUR BODY</h1>
+                        <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo inventore consequatur et at qui ea non mollitia blanditiis debitis fugiat? Nemo tenetur numquam maxime ipsum aspernatur illo magnam ex maiores!</h5>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
